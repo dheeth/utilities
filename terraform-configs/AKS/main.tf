a7db40c14bb70f4a300e147dee8d8d6dc18690e8
Prakarsh <71125043+prakarsh-dt@users.noreply.github.com>
2023-06-19 13:52:31 +0530

    Improved Update main.tf

--- terraform-configs/AKS/main.tf
+++ terraform-configs/AKS/main.tf
@@ -26,96 +26,88 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   resource_group_name = azurerm_resource_group.aks_rg.name
   sku_tier            = "Paid"
   kubernetes_version  = "1.24" # Specify Kubernetes version
-  dns_prefix = "devtron-prod"
-  # Add ssh access configurations for nodes
+  dns_prefix          = "devtron-prod"
+
+  default_node_pool {
+    name                         = "defaultpool"
+    node_count                   = 1
+    vm_size                      = "Standard_DS2_v2"
+    os_disk_size_gb              = 30
+    vnet_subnet_id               = azurerm_subnet.aks_subnet.id
+    enable_auto_scaling          = true
+    min_count                    = 1
+    max_count                    = 5
+    only_critical_addons_enabled = true
+    max_pods_per_node             = 30
+  }
+
   linux_profile {
     admin_username = "ubuntu"
     ssh_key {
       key_data = "ssh-rsa <key-here>"
     }
   }
-  default_node_pool {
-    name                         = "defaultpool"
-    node_count                   = 1
-    min_count                    = 1
-    max_count                    = 1
-    vm_size                      = "Standard_DS2_v2"
-    os_disk_size_gb              = 30
-    only_critical_addons_enabled = true
-    enable_auto_scaling          = true
-  }
+
   identity {
     type = "SystemAssigned"
   }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+    }
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.0.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+    load_balancer_sku = "standard"
+  }
+
   tags = {
     Environment = "Production"
   }
 }
 
-resource "azurerm_kubernetes_cluster_node_pool" "devtron_pool" {
-  name                  = var.devtron_pool_name
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  node_count            = 1
-  min_count             = 1
-  max_count             = 5
-  vm_size               = "Standard_D4as_v5"
-  enable_auto_scaling   = true
-  # Add labels on nodes
-  node_labels = {
-    lifeCycle = "ondemand"
-    purpose   = "prod"
-  }
-  # Specify configuration for kubelet
-  kubelet_config {
-    cpu_manager_policy = "static"
-  }
-  tags = {
-      Environment = "Production"
-      purpose = "prod"
-  }
+# Create the AKS subnet
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.0.0/16"]
 }
 
-resource "azurerm_kubernetes_cluster_node_pool" "ci_pool" {
-  name                  = var.ci_pool_name
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  node_count            = 1
-  min_count             = 1
-  max_count             = 10
-  vm_size               = "Standard_D8s_v5"
-  enable_auto_scaling   = true
-  # enable_node_public_ip = true
-  priority              = "Spot"
-  spot_max_price        = 0.8
-  eviction_policy       = "Delete"
-  # Add labels on nodes
-  node_labels = {
-    purpose   = "ci"
-    "kubernetes.azure.com/scalesetpriority" = "spot"
-  }
-  # Add node taints
-  node_taints = [ "kubernetes.azure.com/scalesetpriority=spot:NoSchedule" ]
-  # Specify configuration for kubelet
-  kubelet_config {
-    cpu_manager_policy = "static"
-  }
-  tags = {
-    Environment = "Production"
-  }
+# Create the AKS virtual network
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "aks-vnet"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
+# Create the log analytics workspace
+resource "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "log-analytics"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location            = azurerm_resource_group.aks_rg.location
+  sku                 = "PerGB2018"
+}
+
+# Create the storage account for blob storage
 resource "azurerm_storage_account" "devtron_blob_storage" {
-  name = var.storage_account_name
-  resource_group_name = var.rg_name
-  location = var.location
-  account_tier = "Standard"
+  name                     = var.storage_account_name
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_nested_items_to_be_public = false
-  public_network_access_enabled = false
-  blob_properties {
-    versioning_enabled = true
+  allow_blob_public_access = false
+
+  lifecycle {
+    prevent_destroy = true
   }
-  network_rules {
-    default_action = "Deny"
-    bypass = [ "AzureServices" ]
+
+  tags = {
+    Environment = "Production"
   }
 }
